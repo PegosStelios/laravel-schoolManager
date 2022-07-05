801c9690aaef4836ca1aa483c5876afc89066333
PegosStelios <steliospegos08@gmail.com>
2022-07-05 07:53:42 +0300

    css wrestling

--- src/resources/views/student.blade.php
+++ src/resources/views/student.blade.php
@@ -2,21 +2,23 @@
 
 @section('content')
 
-<h2>
-    Name: {{$student['name']}} <br>
-    Last Name: {{$student['lastName']}} <br>
-    Mother Name: {{$student['motherName']}} <br>
-    Father Name: {{$student['fatherName']}} <br>
-    Age: {{$student['age']}} <br>
-    Telephone: {{$student['telephone']}} <br>
-    Email: {{$student['email']}} <br>
-    Address: {{$student['address']}} <br>
-    City: {{$student['city']}} <br>
-    Country: {{$student['country']}} <br>
-    Zip COde: {{$student['zip']}} <br>
-    Image: {{$student['image']}} <br>
-    Class: {{$student['class']}} <br>
-    Absent: {{$student['absent']}} <br>
-</h2>
+<div class="singleStudent">
+    <h2>
+        Name: {{$student['name']}} <br>
+        Last Name: {{$student['lastName']}} <br>
+        Mother Name: {{$student['motherName']}} <br>
+        Father Name: {{$student['fatherName']}} <br>
+        Age: {{$student['age']}} <br>
+        Telephone: {{$student['telephone']}} <br>
+        Email: {{$student['email']}} <br>
+        Address: {{$student['address']}} <br>
+        City: {{$student['city']}} <br>
+        Country: {{$student['country']}} <br>
+        Zip COde: {{$student['zip']}} <br>
+        Image: {{$student['image']}} <br>
+        Class: {{$student['class']}} <br>
+        Absent: {{$student['absent']}} <br>
+    </h2>
+</div>
 
 @endsection
